ae4b624b5d88543d641fb9d44350f1b175800991
Isaac <irodriguezsolla@danielcastelao.org>
2026-01-08 14:44:39 +0100

    persona

---
+++ Pesona.py
@@ -0,0 +1,29 @@
+class persoa:
+    def __init__(self,nome,dni,idade):
+        self.setNome(nome)
+        self.__dni = dni
+        self.setIdade(idade)
+
+    def setNome(self,nome):
+        if type(nome) == str:
+            self.__nome = nome
+        else:
+            self.__nome = ("Error")
+    def getNome(self):
+        return self.__nome
+
+    def setIdade(self,idade):
+        if type(idade) == int:
+            self.__idade = idade
+        else:
+            self.__idade = ("Error")
+    def getIdade(self):
+        return self.__idade
+
+    def __str__(self):
+        return self.__nome + " " + str(self.__dni) + " " + str(self.__idade)
+if __name__=='__main__':
+    p = persoa("Alan",554380238,20)
+    try:
+        print(p.__init__())
+    except
