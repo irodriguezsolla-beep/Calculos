924dd7f968df8101cb19226f23aa0be49378a4cc
Isaac <irodriguezsolla@danielcastelao.org>
2026-01-16 10:41:53 +0100

    Ficheros

---
+++ ficheiros.py
@@ -0,0 +1,27 @@
+import datetime
+
+try:
+    ficheiro = open("/home/dam/Documentos/saudo.txt", "r")
+    for linha in ficheiro:
+        print(linha)
+    ficheiro.close()
+except FileNotFoundError as e:
+    print("Erro ao abrir o ficheiro:", e)
+
+# Fecha y hora actual
+data_hora = datetime.datetime.now()
+data, hora = str(data_hora).split()
+
+# Escritura
+ficheiro = open("Editorial.txt", "w")
+ficheiro.write("Ola, son Isaac\n")
+ficheiro.write(data + "\n")
+ficheiro.write(hora + "\n")
+ficheiro.write("Remato e pecho o ficheiro\n")
+ficheiro.close()
+
+# Lectura
+ficheiro = open("Editorial.txt", "r")
+lectura = ficheiro.read()
+print(lectura)
+ficheiro.close()
