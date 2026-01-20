7b02c7f37e1ad097adbd0b4b801811fe2eddfea5
Isaac <irodriguezsolla@danielcastelao.org>
2026-01-20 12:42:35 +0100

    csv2

--- fichero3.py
+++ fichero3.py
@@ -1,7 +1,18 @@
 import csv
 
-with open("exemplo.csv","r")as ficheiro:
+with open("exemplo.csv","w")as ficheiro:
     writer = csv.writer(ficheiro)
     writer.writerow("Nome","Edade","Xenero")
     writer.writerow("Pedro", "23", "Home")
     writer.writerow("Rosa", "25", "Muller")
+
+with open("exemplo2.csv","w")as ficheiro:
+    writer = csv.writer(ficheiro)
+    writer.writerow("Nome"+","+"Edade"+","+"Xenero"+","+"\n")
+    writer.writerow("Pedro"+","+"23"+","+"Home"+","+"\n")
+    writer.writerow("Rosa"+","+"25"+","+"Muller")
+
+with open ("exemplo.csv", "r") as ficheiro:
+    reader = csv.reader(ficheiroro)
+    for fila in reader:
+        print(fila)
